04effd74c4d8012fd9ad550f89bb80c1129bc295
Ansar <ansarh.21@gmail.com>
2024-01-25 16:37:34 +0530

    Complete UI

--- usbank/src/App.js
+++ usbank/src/App.js
@@ -3,17 +3,29 @@ import "./App.css";
 import MyPRofile from "./Components/MyProfile";
 import Account from "./Components/AccountInfo";
 import Dashboard from "./Components/Dashboard";
+import NavBar from "./Components/NavBar";
+import LeftNav from "./LeftNav";
 
 function App() {
   return (
     <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Dashboard />}></Route>
-          <Route path="/account" element={<Account />}></Route>
-          <Route path="/myprofile" element={<MyPRofile />}></Route>
-        </Routes>
-      </BrowserRouter>
+      <NavBar />
+      <div>
+        <div className="row">
+          <div className="col-2">
+            <LeftNav />
+          </div>
+          <div className="col-10">
+            <BrowserRouter>
+              <Routes>
+                <Route path="/" element={<Dashboard />}></Route>
+                <Route path="/account" element={<Account />}></Route>
+                <Route path="/myprofile" element={<MyPRofile />}></Route>
+              </Routes>
+            </BrowserRouter>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
--- usbank/src/Components/Dashboard.jsx
+++ usbank/src/Components/Dashboard.jsx
@@ -1,50 +1,84 @@
 import React, { useState } from "react";
+import { AgGridReact } from "ag-grid-react";
+import "ag-grid-community/styles/ag-grid.css";
+import "ag-grid-community/styles/ag-theme-quartz.css";
 import "./Profile.css";
 
 const Dashboard = () => {
   const [loanData, setLoanData] = useState([
-    { id: 1234567, purpose: "Home Loan", status: "Approved" },
-    { id: 1234567, purpose: "Car Loan", status: "Pending" },
-    { id: 1234567, purpose: "Education Loan", status: "Rejected" },
-    { id: 1234567, purpose: "Personal Loan", status: "Approved" },
-    { id: 1234567, purpose: "Business Loan", status: "Pending" },
+    {
+      id: 1110011,
+      purpose: "Home Loan",
+      status: "Approved",
+      options: "{<button>Save</button>}",
+    },
+    { id: 1210021, purpose: "Car Loan", status: "Pending" },
+    { id: 1120012, purpose: "Education Loan", status: "Rejected" },
+    { id: 133011, purpose: "Personal Loan", status: "Approved" },
+    { id: 1512011, purpose: "Business Loan", status: "Pending" },
+    { id: 1613012, purpose: "Home Loan", status: "Approved" },
+    { id: 1724013, purpose: "Car Loan", status: "Rejected" },
+    { id: 1835014, purpose: "Education Loan", status: "Pending" },
+    { id: 1946015, purpose: "Personal Loan", status: "Approved" },
+    { id: 2057016, purpose: "Business Loan", status: "Pending" },
+    { id: 2168017, purpose: "Home Loan", status: "Rejected" },
+    { id: 2279018, purpose: "Car Loan", status: "Approved" },
+    { id: 2390019, purpose: "Education Loan", status: "Pending" },
+    { id: 241010, purpose: "Personal Loan", status: "Approved" },
+    { id: 252110, purpose: "Business Loan", status: "Rejected" },
+    { id: 263210, purpose: "Home Loan", status: "Pending" },
+    { id: 274310, purpose: "Car Loan", status: "Approved" },
+    { id: 285410, purpose: "Education Loan", status: "Pending" },
+    { id: 296510, purpose: "Personal Loan", status: "Approved" },
   ]);
 
   const handleSave = (id) => {
     console.log(`Saving data for loan ID: ${id}`);
   };
 
+  const columnDefs = [
+    { headerName: "Loan ID", field: "id", filter: "agTextColumnFilter" },
+    {
+      headerName: "Purpose of Loan",
+      field: "purpose",
+      filter: "agTextColumnFilter",
+    },
+    { headerName: "Status", field: "status", filter: "agTextColumnFilter" },
+    {
+      headerName: "Action",
+      field: "price",
+      cellRenderer: () => (
+        <div>
+          <button className="btn-next ">Save</button>
+        </div>
+      ),
+    },
+  ];
+
+  const frameworkComponents = {
+    optionsRenderer: (props) => (
+      <button className="btn-next" onClick={() => handleSave(props.data.id)}>
+        Save
+      </button>
+    ),
+  };
+
   return (
-    <div className="container">
-      <h2 className="heading-text text-grey">Loan Dashboard</h2>
-      <h4 className="heading-text-smaller text-grey">Welcome User</h4>
-      <table className="dashboard-table">
-        <thead>
-          <tr>
-            <th className="text-grey">Loan ID</th>
-            <th className="text-grey">Purpose of Loan</th>
-            <th className="text-grey">Status</th>
-            <th className="text-grey">Options</th>
-          </tr>
-        </thead>
-        <tbody>
-          {loanData.map((loan) => (
-            <tr key={loan.id}>
-              <td className="text-grey">{loan.id}</td>
-              <td className="text-grey">{loan.purpose}</td>
-              <td className="text-grey">{loan.status}</td>
-              <td>
-                <button
-                  className="btn-next"
-                  onClick={() => handleSave(loan.id)}
-                >
-                  Save
-                </button>
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+    <div className="container-dashboard">
+      <h4 className="heading-text-smaller text-grey text-center pd20">
+        Welcome User
+      </h4>
+      <div
+        className="ag-theme-quartz"
+        style={{ height: "300px", width: "100%" }}
+      >
+        <AgGridReact
+          columnDefs={columnDefs}
+          rowData={loanData}
+          frameworkComponents={frameworkComponents}
+          domLayout="autoHeight"
+        />
+      </div>
     </div>
   );
 };
